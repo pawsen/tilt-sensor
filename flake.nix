067ccb09621888cdb52fe0b6aefdd896c78d71f8
Paw Møller <pawsen@gmail.com>
2024-11-19 18:47:31 +0100

    feat: solely use flake.nix
    Before, where flake.nix would call shell.nix in devShell, the shell
    would only be setup correctly if invoked explicitly by `nix develop -c $SHELL`
    It would not work with direnv.
    
    Combining everything into flake.nix, the direnv invocation works.

--- flake.nix
+++ flake.nix
@@ -1,19 +1,35 @@
+# use with
+# nix develop -c $SHELL
 {
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  };
+  inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; };
 
   outputs = { nixpkgs, ... }:
-    let
-      forAllSystems = nixpkgs.lib.genAttrs [ "aarch64-linux" "x86_64-linux" ];
-    in
-    {
-      devShells = forAllSystems (system: {
-        default = nixpkgs.legacyPackages.${system}.callPackage ./shell.nix { };
-      });
+    let forAllSystems = nixpkgs.lib.genAttrs [ "aarch64-linux" "x86_64-linux" ];
+    in {
+      devShells = forAllSystems (system:
+        let pkgs = import nixpkgs { inherit system; };
+        in with pkgs; rec {
+          default = mkShell {
+
+            ARDMK_DIR = pkgs.arduino-mk;
+            ARDUINO_DIR = "${pkgs.arduino-core-unwrapped}/share/arduino";
+            ARDUINO_LIB_PATH =
+              "${pkgs.arduino-core-unwrapped}/share/arduino/libraries";
+            buildInputs = with pkgs; [
+              (python3.withPackages (ps: with ps; [ pyserial ]))
+              arduino-core-unwrapped
+              arduino-mk
+              # use tio instead of screen, tio -b 9600 /dev/ttyUSB0
+              tio
+							# keep screen for make monitor
+              screen
+            ];
+
+          };
+        });
       packages = forAllSystems (system: {
-        default = nixpkgs.legacyPackages.${system}.callPackage ./default.nix { };
+        default =
+          nixpkgs.legacyPackages.${system}.callPackage ./default.nix { };
       });
     };
 }
-
