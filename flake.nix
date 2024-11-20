f6bcf7be44771e8d4eb07ab4bb87cc571c4cf308
Paw Møller <pawsen@gmail.com>
2024-11-20 19:03:37 +0100

    feat: update flake.nix following a template
    https://github.com/the-nix-way/dev-templates

--- flake.nix
+++ flake.nix
@@ -1,35 +1,50 @@
 # use with
 # nix develop -c $SHELL
 {
+  description = "nix flake for arduino MK";
   inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; };
-
-  outputs = { nixpkgs, ... }:
-    let forAllSystems = nixpkgs.lib.genAttrs [ "aarch64-linux" "x86_64-linux" ];
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems =
+        [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+			# Helper to provide system-specific attributes
+      forEachSupportedSystem = f:
+        nixpkgs.lib.genAttrs supportedSystems
+        (system: f { pkgs = import nixpkgs { inherit system; }; });
     in {
-      devShells = forAllSystems (system:
-        let pkgs = import nixpkgs { inherit system; };
-        in with pkgs; rec {
-          default = mkShell {
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell.override {
+          # Override stdenv in order to change compiler:
+          # stdenv = pkgs.clangStdenv;
+        }
 
-            ARDMK_DIR = pkgs.arduino-mk;
-            ARDUINO_DIR = "${pkgs.arduino-core-unwrapped}/share/arduino";
-            ARDUINO_LIB_PATH =
-              "${pkgs.arduino-core-unwrapped}/share/arduino/libraries";
-            buildInputs = with pkgs; [
+          {
+
+            # The Nix packages provided in the environment
+            packages = with pkgs; [
               (python3.withPackages (ps: with ps; [ pyserial ]))
               arduino-core-unwrapped
               arduino-mk
               # use tio instead of screen, tio -b 9600 /dev/ttyUSB0
               tio
-							# keep screen for make monitor
+              # keep screen for make monitor
               screen
             ];
 
+            # buildInputs = with pkgs; [];
+
+            # Set any environment variables for your dev shell
+            env = {
+              ARDMK_DIR = pkgs.arduino-mk;
+              ARDUINO_DIR = "${pkgs.arduino-core-unwrapped}/share/arduino";
+              ARDUINO_LIB_PATH =
+                "${pkgs.arduino-core-unwrapped}/share/arduino/libraries";
+            };
+
+            # Add any shell logic you want executed any time the environment is activated
+            shellHook = ''
+						'';
           };
-        });
-      packages = forAllSystems (system: {
-        default =
-          nixpkgs.legacyPackages.${system}.callPackage ./default.nix { };
       });
     };
 }
